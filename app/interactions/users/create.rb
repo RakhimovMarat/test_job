a76722dc07b980c796d2dad0a34ff07844abf83f
marat <mf.rakhimov1982@gmail.com>
2024-09-21 23:32:17 +0300

    Update Interactor Model and Spec

--- app/interactions/users/create.rb
+++ app/interactions/users/create.rb
@@ -1,22 +1,28 @@
 class Users::Create < ActiveInteraction::Base
-  string  :name, :surname, :patronymic, :email, :nationality, :country, :full_name, :gender
+  string  :name, :surname, :patronymic, :email, :nationality, :country, :gender, :full_name
   integer :age
   array   :interests, default: []
   array   :skills, default: []
 
   validates :name, :surname, :patronymic, :email, :age, :nationality, :country, :gender, :full_name, presence: true
-  validates :age, numericality: true, inclusion: { in: 1..90 }
+  validates :age, numericality: { greater_than: 0, less_than_or_equal_to: 90 }
   validates :gender, inclusion: { in: %w[male female] }
 
+  set_callback :execute, :after do
+    if @user.persisted?
+      add_interests(@user)
+      add_skills(@user)
+    end
+  end
+
   def execute
-    user = create_user
-    add_interests(user)
-    add_skills(user)
+    @user = create_user
+    @user
   end
 
   def create_user
     user_full_name = "#{surname} #{name} #{patronymic}"
-    User.create!(
+    User.create(
       name: name,
       surname: surname,
       patronymic: patronymic,
@@ -30,10 +36,10 @@ class Users::Create < ActiveInteraction::Base
   end
 
   def add_interests(user)
-    user.interests << Interest.where(name: interests) if interests.present?
+    user.interests.concat(Interest.where(name: interests)) if interests.present?
   end
 
   def add_skills(user)
-    user.skills << Skill.where(name: skills) if skills.present?
+    user.skills.concat(Skill.where(name: skills)) if skills.present?
   end
 end
--- spec/interactions/create_spec.rb
+++ spec/interactions/create_spec.rb
@@ -1,65 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Users::Create, type: :interactor do
-  subject { create :user }
+  let(:user_attributes) { attributes_for(:user) }
+  let(:skills)          { create_list(:skill, 2).map(&:name) }
+  let(:interests)       { create_list(:interest, 2).map(&:name) }
 
   describe 'user' do
-    it 'create new user' do
-      expect(subject).to be_persisted
+    subject { described_class.run(user_attributes) }
+
+    it 'user is succesfully created' do
+      expect(subject.result).to be_persisted
     end
 
     it 'user has valid attributes' do
-      expect(subject).to have_attributes(
-        name:        a_kind_of(String),
-        surname:     a_kind_of(String),
-        patronymic:  a_kind_of(String),
-        email:       a_kind_of(String),
-        age:         a_kind_of(Integer),
-        nationality: a_kind_of(String),
-        country:     a_kind_of(String),
-        gender:      a_kind_of(String),
-        full_name:   a_kind_of(String)
-      )
+      expect(subject.result).to have_attributes(user_attributes)
     end
 
     it 'user age is within 1 and 90' do
-      expect(subject.age).to be_between(1, 90)
+      expect(subject.result.age).to be_between(1, 90)
     end
 
     it 'user has valid gender' do
-      expect(%w[male female]).to include(subject.gender)
+      expect(%w[male female]).to include(subject.result.gender)
     end
+  end
+
+  describe 'arrays' do
+    subject { described_class.run(user_attributes) }
 
     it 'user interests as an array' do
-      expect(subject.interests.to_a).to be_an(Array)
+      expect(subject.result.interests.to_a).to be_an(Array)
     end
 
     it 'user skills as an array' do
-      expect(subject.skills.to_a).to be_an(Array)
+      expect(subject.result.skills.to_a).to be_an(Array)
     end
   end
 
-  describe 'interest' do
-    let(:interests) { create_list(:interest, 2) }
-
-    before do
-      subject.interests << interests
-    end
+  describe 'interests and skills' do
+    subject { described_class.run(user_attributes.merge(interests: interests, skills: skills)) }
 
     it 'add interests to user' do
-      expect(subject.interests).to match_array(interests)
-    end
-  end
-
-  describe 'skill' do
-    let(:skills) { create_list(:skill, 2) }
-
-    before do
-      subject.skills << skills
+      expect(subject.result.interests.map(&:name)).to match_array(interests)
     end
 
     it 'add skills to user' do
-      expect(subject.skills).to match_array(skills)
+      expect(subject.result.skills.map(&:name)).to match_array(skills)
     end
-  end
+   end
 end
